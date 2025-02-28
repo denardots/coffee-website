ae12e615b18ea1390d116aa0c3b19570a07857d1
Denardo Taco Sánchez <129450477+denardots@users.noreply.github.com>
2025-02-28 11:38:23 -0500

    feat: Create the HTML structure

---
+++ stylelint.config.js
@@ -0,0 +1,15 @@
+export default {
+  extends: "stylelint-config-standard",
+  rules: {
+    "no-unknown-animations": true,
+    "no-unknown-custom-media": true,
+    "no-unknown-custom-properties": true,
+    "at-rule-property-required-list": {"font-face": ["font-display", "font-family", "font-style"]},
+    "color-named": "never",
+    "declaration-no-important": true,
+    "comment-empty-line-before": null,
+    "custom-property-empty-line-before": "never",
+    "at-rule-empty-line-before": "never",
+    "rule-empty-line-before": "never"
+  }
+};

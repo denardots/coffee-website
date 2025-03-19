d52a037b769496c4a7677c3ae585a66682c12384
Denardo Taco Sánchez <129450477+denardots@users.noreply.github.com>
2025-03-18 21:19:12 -0500

    refactor: Refactor the styles of the project.

--- README.md
+++ README.md
@@ -1,9 +1,34 @@
-# Coffee Website
+# Proyecto Coffee Website
 
-"@html-eslint/eslint-plugin": "^0.35.2",
-"@html-eslint/parser": "^0.35.2",
-"eslint": "^9.22.0",
-"eslint-config-standard": "^17.1.0",
-"eslint-plugin-import": "^2.31.0",
-"stylelint": "^16.15.0",
-"stylelint-config-standard": "^37.0.0",
+## Descripción
+
+El proyecto consiste en una landing page de una cafeteria.
+
+## Tecnologías utilizadas
+
+* HTML
+* CSS
+* JavaScript
+
+## Librerías utilizadas
+
+* [Swiper](https://swiperjs.com/ "Swiper")
+
+## Fuentes utilizadas
+
+* [Poppins](https://fonts.google.com/specimen/Poppins "Google Fonts")
+* [Miniver](https://fonts.google.com/specimen/Miniver "Google Fonts")
+
+### Dependencias de linting y formateo en desarrollo (OPCIONAL)
+
+```json
+  "devDependencies": {
+    "@html-eslint/eslint-plugin": "^0.35.2",
+    "@html-eslint/parser": "^0.35.2",
+    "eslint": "^9.22.0",
+    "eslint-config-standard": "^17.1.0",
+    "eslint-plugin-import": "^2.31.0",
+    "stylelint": "^16.15.0",
+    "stylelint-config-standard": "^37.0.0",
+  }
+```
--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
+"use strict";
+
 import Swiper from "swiper";
 import {Navigation, Pagination} from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
+import "./css/style.css";
 
 const header = document.getElementById("header");
 const nav = document.getElementById("nav");
@@ -10,35 +13,42 @@ const menuCloseButton = document.getElementById("menuCloseButton");
 const menuOpenButton = document.getElementById("menuOpenButton");
 const navLinks = document.querySelectorAll(".header__nav__list__link");
 
+// Listener para abrir el menú
 menuOpenButton.addEventListener("click", () => {
   nav.classList.toggle("header__nav--active");
   header.classList.toggle("header--inactive");
 });
 
+// Listener para cerrar el menú
 menuCloseButton.addEventListener("click", () => menuOpenButton.click());
 
+// Listener para cerrar el menú al hacer clic en un enlace
 navLinks.forEach(link => link.addEventListener("click", () => menuOpenButton.click()));
 
-// eslint-disable-next-line no-unused-vars
+// Instanciamos la clase Swiper y la configuramos para el slider
 const swiper = new Swiper(".swiper", {
+  // Agregamos los modulos de Navegación y Paginación
   modules: [Navigation, Pagination],
+  // Definimos el modo de deslizar
   loop: true,
   grabCursor: true,
-  spacesBetween: 25,
+  // Definimos la paginación
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
     dynamicBullets: true
   },
+  // Definimos los botones de navegación
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev"
   },
+  // Definimos la cantidad de slides por pantalla
   breakpoints: {
     0: {
       slidesPerView: 1
     },
-    768: {
+    700: {
       slidesPerView: 2
     },
     1024: {
@@ -46,3 +56,6 @@ const swiper = new Swiper(".swiper", {
     }
   }
 });
+
+// Definimos el espacio entre los slides
+swiper.spacesBetween = 25;
